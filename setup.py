1d016ecbf5d2629614e6e7df96830f226b39da93
Tao Li <lp1559345469@gmail.com>
2020-10-23 12:24:21 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,3 @@
-#! /usr/bin/env python
-#
-# Copyright (C) 2007-2009 Cournapeau David <cournape@gmail.com>
-#               2010 Fabian Pedregosa <fabian.pedregosa@inria.fr>
-# License: 3-clause BSD
-
 import sys
 import os
 import platform
